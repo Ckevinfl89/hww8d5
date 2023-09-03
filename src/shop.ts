c97e754f577b227bf56b4a375e214ede5fc00df4
christian sanabria <ckevin89@hotmail.com>
2023-09-03 14:30:51 -0500

    update

--- src/shop.ts
+++ src/shop.ts
@@ -7,7 +7,7 @@ export class Shop {
     this.items = [];
     const boxenOfDoughnuts = new Item("Dozen Doughnuts", 7.99, "Buy the holes too. Don't leave em hangin'.");
     const canOpener = new Item("The opener of cans", 1.99, "Has spinny blades with handles.");
-    const cheese = new Item("Tillamook Cheese: Medium", 8.99, "Medium. Solid creamy goodness.");
+    const cheese = new Item("Cheese", 8.99, "Solid creamy goodness.");
     this.items.push(boxenOfDoughnuts, canOpener, cheese);
   }
 
